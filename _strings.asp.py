7984fc1c516b04a520383bbafc4885a8910c0d4f
Toleu Bahauddin <baha200477@gmail.com>
2023-01-18 20:13:56 +0600

    First Commit

---
+++ _strings.asp.py
@@ -0,0 +1,45 @@
+print("Hello")
+print('Hello')
+
+a = "Hello"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+
+#second element
+a = "Hello, World!"
+print(a[1])
+
+#выводит по буквам
+for x in "banana":
+  print(x)
+
+#length
+a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
---
+++ _syntax.asp.py
@@ -0,0 +1,17 @@
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+    print("Five is greater than two!") 
+
+
+# Mistake errors
+# if 5 > 2:
+#  print("Five is greater than two!")
+#         print("Five is greater than two!")
+
+#Mistake errors
+# if 5 > 2:
+# print("Five is greater than two!")
+
+if 5 > 2:
+  print("Five is greater than two!")
